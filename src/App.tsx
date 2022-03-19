f60795ac984de256496b7841a0128d8efbcc7b6a
Simon <simonbesteiro@gmail.com>
2022-03-19 10:26:12 +0100

    show current date

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+         This is working
         </p>
+        <p>{new Date().toLocaleString()}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
